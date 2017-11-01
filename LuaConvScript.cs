50eb4e7e1aa3a5c26fd6a1a537879e178612b6e2
Nigel Hardy <nigeljames.12@gmail.com>
2017-11-01 10:56:51 -0700

    Integrated Scott and Phucs code and fixed some function definitions

--- LuaConvScript.cs
+++ LuaConvScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using MoonSharp.Interpreter;
 
-public class LuaConvScript : MonoBehaviour {
+public class LuaConvScript : MonoBehaviour
+{
     int state = 0;
     playerMovement pmScript;
     GameManager gmScript;
@@ -15,7 +16,7 @@ public class LuaConvScript : MonoBehaviour {
     public int InputSize;
 
     List<string> ButtonNames = new List<string> { "Left", "Right" };
-    int Inputs; 
+    int Inputs;
     int Outputs;
 
     public int Population = 300;
@@ -41,7 +42,7 @@ public class LuaConvScript : MonoBehaviour {
     public int MaxNodes = 1000000;
 
     private double marioX, marioY;
-
+    private double rightmost = 0;
     // values from game that can be used
     //gmScript.health
     //gmScript.gameOver
@@ -90,7 +91,7 @@ public class LuaConvScript : MonoBehaviour {
             mutationRates["disable"] = DisableMutationChance;
             mutationRates["step"] = StepSize;
         }
-        
+
     }
     class species
     {
@@ -103,10 +104,11 @@ public class LuaConvScript : MonoBehaviour {
         {
             topFitness = 0;
             staleness = 0;
-            List<genome> genomes;
+            genomes = new List<genome>();
             averageFitness = 0;
         }
     }
+
     class pool
     {
         public List<species> species;
@@ -128,13 +130,14 @@ public class LuaConvScript : MonoBehaviour {
 
         public neuron()
         {
-            List<int> incoming = new List<int>();
+            incoming = new List<int>();
             value = 0.0;
         }
     }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         pmScript = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovement>();
         gmScript = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         tsScript = GameObject.Find("trashLocation").GetComponent<trashSpawn>();
@@ -152,6 +155,76 @@ public class LuaConvScript : MonoBehaviour {
         removeNull();
         getPositions();
 
+        pool pool = initializePool();
+
+        species species = pool.species[pool.currentSpecies];
+        genome genome = species.genomes[pool.currentGenome];
+
+        if (pool.currentFrame % 5 == 0)
+        {
+            evaluateCurrent(pool);
+        }
+
+        //Don't know what to do with this
+        //joypad.set(controller);
+
+        getPositions();
+        if (marioX > rightmost)
+        {
+            rightmost = marioX;
+            timeout = TimeoutConstant;
+        }
+
+        timeout = timeout - 1;
+
+        double timeoutBonus = pool.currentFrame / 4;
+        if (timeout + timeoutBonus <= 0)
+        {
+            double fitness = rightmost - pool.currentFrame / 2;
+            if (rightmost > 4816)
+            {
+                fitness += 1000;
+            }
+            if (rightmost > 3186)
+            {
+                fitness += 1000;
+            }
+            if (fitness == 0)
+            {
+                fitness = -1;
+            }
+
+            genome.fitness = fitness;
+
+            if (fitness > pool.maxFitness)
+            {
+                pool.maxFitness = fitness;
+            }
+
+            pool.currentSpecies = 1;
+            pool.currentGenome = 1;
+            while (fitnessAlreadyMeasured(pool))
+            {
+                nextGenome(pool);
+            }
+            initializeRun(pool);
+        }
+
+        double measured = 0;
+        double total = 0;
+        for (int i = 0; i < pool.species.Count; i++)
+        {
+            for (int j = 0; j < species.genomes.Count; j++)
+            {
+                total += 1;
+                if (genome.fitness != 0)
+                {
+                    measured += 1;
+                }
+            }
+        }
+
+        pool.currentFrame += 1;
 
         if (!pmScript.ai_testing || true)
         {
@@ -172,7 +245,7 @@ public class LuaConvScript : MonoBehaviour {
             }
             stateToMovement(state);
         }
-        
+
         if (gmScript.gameOver)
         {
             restartLevel();
@@ -238,7 +311,7 @@ public class LuaConvScript : MonoBehaviour {
         foreach (var item in neurons)
         {
             n = n - 1;
-            if(n == 0)
+            if (n == 0)
             {
                 return item.Key;
             }
@@ -251,7 +324,7 @@ public class LuaConvScript : MonoBehaviour {
         for (int i = 0; i < g.genes.Count; i++)
         {
             gene geneLocal = g.genes[i];
-            if(Random.Range(0.0f, 1.0f) < PerturbChance)
+            if (Random.Range(0.0f, 1.0f) < PerturbChance)
             {
                 geneLocal.weight = geneLocal.weight + Random.Range(0.0f, 1.0f) * step * 2 - step;
             }
@@ -314,27 +387,27 @@ public class LuaConvScript : MonoBehaviour {
     {
         if (g.genes.Count == 0)
             return;
-    
-            g.maxneuron = g.maxneuron + 1;
-    
-            int rand = Random.Range(1, g.genes.Count);
-            var gene = g.genes[rand];
-            if (!gene.enabled)
-                return;
-            gene.enabled = false;
-    
-            gene gene1 = copyGene(gene);
-            gene1.Out = g.maxneuron;
-            gene1.weight = 1.0;
-            gene1.innovation += 1;
-            gene1.enabled = true;
-            g.genes.Add(gene1);
 
-            gene gene2 = copyGene(gene);
-            gene2.into = g.maxneuron;
-            gene2.innovation += 1;
-            gene2.enabled = true;
-            g.genes.Add(gene2);
+        g.maxneuron = g.maxneuron + 1;
+
+        int rand = Random.Range(1, g.genes.Count);
+        var gene = g.genes[rand];
+        if (!gene.enabled)
+            return;
+        gene.enabled = false;
+
+        gene gene1 = copyGene(gene);
+        gene1.Out = g.maxneuron;
+        gene1.weight = 1.0;
+        gene1.innovation += 1;
+        gene1.enabled = true;
+        g.genes.Add(gene1);
+
+        gene gene2 = copyGene(gene);
+        gene2.into = g.maxneuron;
+        gene2.innovation += 1;
+        gene2.enabled = true;
+        g.genes.Add(gene2);
     }
     void mutate(genome g)
     {
@@ -342,7 +415,7 @@ public class LuaConvScript : MonoBehaviour {
         foreach (KeyValuePair<string, double> entry in g.mutationRates)
         {
             // do something with entry.Value or entry.Key
-            if(Mathf.RoundToInt(Random.Range(0,2)) == 1)
+            if (Mathf.RoundToInt(Random.Range(0, 2)) == 1)
             {
                 g.mutationRates[entry.Key] = .95 * entry.Value;
             }
@@ -351,14 +424,14 @@ public class LuaConvScript : MonoBehaviour {
                 g.mutationRates[entry.Key] = 1.05263 * entry.Value;
             }
         }
-        if(Random.Range(0.0f, 1.0f) < g.mutationRates["connections"])
+        if (Random.Range(0.0f, 1.0f) < g.mutationRates["connections"])
         {
             pointMutate(g);
         }
         double p = g.mutationRates["link"];
         while (p > 0)
         {
-            if(Random.Range(0.0f, 1.0f) < p)
+            if (Random.Range(0.0f, 1.0f) < p)
             {
                 linkMutate(g, false);
             }
@@ -409,7 +482,7 @@ public class LuaConvScript : MonoBehaviour {
     }
     genome basicGenome()
     {
-        genome bg =  newGenome();
+        genome bg = newGenome();
         // not sure how this innovation variable is useful??
         double innovation = 1;
         bg.maxneuron = Inputs;
@@ -458,7 +531,7 @@ public class LuaConvScript : MonoBehaviour {
         List<Transform> sprites = new List<Transform>();
         for (int i = 0; i < tsScript.trash.Count; i++)
         {
-            
+
         }
         // positions of recycleables
         for (int i = 0; i < tsScript.recycleable.Count; i++)
@@ -486,21 +559,21 @@ public class LuaConvScript : MonoBehaviour {
 
         //inputs = {}
         List<int> inputs = new List<int>();
-        for (int dy = -BoxRadius*16; dy < BoxRadius*16; dy=dy+16)
+        for (int dy = -BoxRadius * 16; dy < BoxRadius * 16; dy = dy + 16)
         {
-            for (int dx = -BoxRadius*16; dx < BoxRadius*16; dx = dx + 16)
+            for (int dx = -BoxRadius * 16; dx < BoxRadius * 16; dx = dx + 16)
             {
                 inputs.Add(0);
                 int tile = getTile(dx, dy);
-                if(tile == 1 && marioY+dy < 0x1B0)
+                if (tile == 1 && marioY + dy < 0x1B0)
                 {
                     inputs.Add(1);
                 }
                 for (int i = 0; i < sprites.Count; i++)
                 {
-                    double distx = Mathf.Abs(sprites[i].position.x) - (marioX + (double) dx);
+                    double distx = Mathf.Abs(sprites[i].position.x) - (marioX + (double)dx);
                     double disty = Mathf.Abs(sprites[i].position.y) - (marioY + (double)dy);
-                    if(distx <= 8 && disty <= 8)
+                    if (distx <= 8 && disty <= 8)
                     {
                         inputs.Add(-1);
                     }
@@ -542,7 +615,7 @@ public class LuaConvScript : MonoBehaviour {
         return new pool(Outputs);
     }
     // END Nigel's work
-    
+
     // Phucs Work
     species newSpecies()
     {
@@ -559,8 +632,8 @@ public class LuaConvScript : MonoBehaviour {
         return new neuron();
     }
     // End Phucs work
-    
-    
+
+
     // Begin Daniel's Work
     void playTop()
     {
@@ -573,10 +646,10 @@ public class LuaConvScript : MonoBehaviour {
 
         gen.network = network;
     }
-    Dictionary<int, bool> evaluateNetwork(Dictionary<int, neuron> network, int inputs)
+    Dictionary<string, bool> evaluateNetwork(Dictionary<int, neuron> network, List<int> inputs)
     {
         // to do
-        Dictionary<int, bool> outputs = new Dictionary<int, bool>();
+        Dictionary<string, bool> outputs = new Dictionary<string, bool>();
 
 
         return outputs;
@@ -608,8 +681,8 @@ public class LuaConvScript : MonoBehaviour {
         return 0;
     }
     // END Daniel's Work
-    
-    
+
+
     // Being Dustin's Work
     void rankGlobally()
     {
@@ -632,7 +705,7 @@ public class LuaConvScript : MonoBehaviour {
     {
         // to do
     }
-    void addToSpecies(species child)
+    void addToSpecies(genome child)
     {
         // to do
     }
@@ -646,41 +719,102 @@ public class LuaConvScript : MonoBehaviour {
     }
 
     // End 8=Dustin's Work
-    
+
     // BEING Scott's Work
-    void initializePool()
+    pool initializePool()
+    {
+        pool pool = new pool(Outputs);
+
+        for (int i = 0; i < Population; i++)
+        {
+            genome basic = basicGenome();
+            addToSpecies(basic);
+        }
+
+        initializeRun(pool);
+        return pool;
+    }
+
+    void initializeRun(pool pool)
+    {
+        // Still need?
+        //savestate.load(Filename);
+        rightmost = 0;
+        pool.currentFrame = 0;
+        timeout = TimeoutConstant;
+        clearJoypad();
+
+        species species = pool.species[pool.currentSpecies];
+        genome genome = species.genomes[pool.currentGenome];
+        evaluateCurrent(pool);
+    }
+
+    void evaluateCurrent(pool pool)
+    {
+        species species = pool.species[pool.currentSpecies];
+        genome genome = species.genomes[pool.currentGenome];
+
+        List<int> inputs = getInputs();
+        Dictionary<string, bool> controller = evaluateNetwork(genome.network, inputs);
+
+        if (controller["P1 Left"] && controller["P1 Right"])
+        {
+            controller["P1 Left"] = false;
+            controller["P1 Right"] = false;
+        }
+        if (controller["P1 Up"] && controller["P1 Down"])
+        {
+            controller["P1 Up"] = false;
+            controller["P1 Down"] = false;
+        }
+
+        //Is there a joypad class? Where is this in our current stuff??
+        //joypad.set(controller);
+    }
+
+    void nextGenome(pool pool)
+    {
+        pool.currentGenome = pool.currentGenome + 1;
+        if (pool.currentGenome > pool.species[pool.currentSpecies].genomes.Count)
+        {
+            pool.currentGenome = 1;
+            pool.currentSpecies = pool.currentSpecies + 1;
+            if (pool.currentSpecies > pool.species.Count)
+            {
+                newGeneration();
+                pool.currentSpecies = 1;
+            }
+        }
+    }
+
+    bool fitnessAlreadyMeasured(pool pool)
+    {
+        species species = pool.species[pool.currentSpecies];
+        genome genome = species.genomes[pool.currentGenome];
+
+        return genome.fitness != 0;
+    }
+
+    //Might not need these last three functions
+    //If we do then we need to find a replacement for 'forms' in lua
+    void writeFile(string filename)
     {
         // to do
+
     }
-    
-    void initializeRun()
-    {
-        // to do
-    }
-    void evaluateCurrent()
-    {
-        // to do
-    }
-    void nextGenome()
-    {
-        // to do
-    }
-    bool fitnessAlreadyMeasured()
-    {
-        // to do
-        return false;
-    }
-    void writeFile()
-    {
-        // to do
-    }
+
     void savePool()
     {
         // to do
+        string filename = "";
+        writeFile(filename);
     }
+
     void loadPool()
     {
         // to do
+        string filename = "";
+        //loadFile(filename);
     }
     // End Scott's Work
 
@@ -708,7 +842,6 @@ public class LuaConvScript : MonoBehaviour {
 				        return n*fact(n - 1)
 			        end
 		        end
-
 		        return fact({0})";
         script = string.Format(script, tempIn);
         DynValue res = Script.RunString(script);
